6297a7ab77060174599ccb2b42dd96dcda65ce9b
Ryan Campbell <ryancampbell@berkeley.edu>
2022-01-02 01:07:57 -0800

    axis

--- grapher.py
+++ grapher.py
@@ -3,5 +3,12 @@ import turtle
 window = turtle.Screen()
 celine = turtle.Turtle()
 
+def draw_axis(rotation):
+    t = turtle.Turtle()
+    t.left(rotation)
+    t.forward(max(window.window_width(), window.window_height()) / 2)
+    t.backward(max(window.window_width(), window.window_height()))
 
+draw_axis(0) # x-axis
+draw_axis(90) # y-axis
 
